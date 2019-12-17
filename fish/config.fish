cf4b31e1c6f2820a887406383edb563f24bb634f
Jorge Luis L. Amorim <lopesluisjorge@hotmail.com>
2019-12-17 08:34:16 -0300

    fish shell

---
+++ fish/config.fish
@@ -0,0 +1,2 @@
+export PATH="$PATH:$HOME/.opt/node/bin"
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
---
+++ fish/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# backup :)
+mkdir -p $HOME/.config/fish
+
+cp config.fish $HOME/.config/fish
+
+source $HOME/.config/fish/config.fish
